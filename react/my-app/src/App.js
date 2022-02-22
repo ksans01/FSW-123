7a99342c571b2e8a0013bc3c141209ebdb5ae504
ksans01 <kurt.sanson@bryanuniversity.edu>
2022-02-21 23:09:07 -0600

    Initial

---
+++ react/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Greeting from './greeting';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Greeting />
+      <Greeting />
+      <Greeting />
+    </>
+    );
+}
+
+export default App;
+
---
+++ react/my-app/src/greeting.js
@@ -0,0 +1,11 @@
+
+
+function Greeting() {
+    const greeting = "Good morning!"
+    return (
+        // Anytime you want to put javascript JSX you have to put curly brackets inside html tags
+        <h1>{greeting}</h1>
+    );
+}
+
+export default Greeting;
