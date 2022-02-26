a56ee8a5f9f069b10807b91b501a57ac2926aade
ksans01 <kurt.sanson@bryanuniversity.edu>
2022-02-26 17:03:53 -0600

    todo list

---
+++ week1/todo-list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './TodoList.js';
+
+TodoList()
+function App() {
+  return (
+      <div>
+        <h1><TodoList/></h1>
+      </div>
+    );
+}
+
+export default App;
---
+++ week1/todo-list/src/TodoList.js
@@ -0,0 +1,18 @@
+
+
+export default function TodoList () {
+    return (
+    
+        <div>
+            <h1>List of Todos</h1>
+                <ul>
+                    <input type="checkbox" /> Go to gym<br></br>
+                    <input type="checkbox" /> Do laundry<br></br>
+                    <input type="checkbox" /> Food shopping<br></br>
+                    <input type="checkbox" /> Do homework<br></br>
+                </ul>          
+                        
+        </div>)
+}
+
+TodoList()
